53e1ab572366d5c24ed509ae1719d7d8e655372b
jhon0596 <jrodriguez050796@gmail.com>
2018-08-01 14:55:00 -0600

    pruebas arduino

---
+++ Pruebas/PruebaSensores1/PruebaComunicacionLED/PruebaComunicacionLED.ino
@@ -0,0 +1,18 @@
+#define LED 13 // pin 13
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED,OUTPUT);
+}
+
+void loop() {
+  if(Serial.available() > 0){
+    int input = Serial.read();
+    if (input == '1'){
+      digitalWrite(13, HIGH);
+    }
+    else{
+      digitalWrite(13,LOW);
+    }
+  }
+}
---
+++ Pruebas/PruebaSensores1/PruebaSensores1.ino
@@ -0,0 +1,33 @@
+#include <DHT.h>
+
+#define LDR 0 //pin A0
+#define DHTPIN 2 //pin 2
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+  pinMode(LDR,INPUT);
+  pinMode(DHTPIN,INPUT);
+}
+
+void loop() {
+  int valor_hum = dht.readHumidity();
+  int valor_temp = dht.readTemperature();
+  int valor_luz = analogRead(LDR);
+  int luz = (100-((1.0 * valor_luz * 100) /1024));
+
+  String output = " ";
+  output += luz;
+  output += " ";
+  output += valor_temp;
+  output += " ";
+  output += valor_hum;
+  output += " ";
+
+  Serial.println(output);
+  delay (1000);
+}
